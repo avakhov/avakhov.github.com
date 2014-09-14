ddefef9f79f119c15e5d45e337f0db7598cf6b18
Alexey Vakhov <vakhov@gmail.com>
2014-09-14 12:39:17 +0400

    Fix feed + cosmetic

--- push_test.sh
+++ push_test.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+
+echo "baseurl: /vakhov-test" >> _config.yml
 bundle exec jekyll build
+cp _config.yml _config.yml.tmp
+sed '$ d' _config.yml.tmp > _config.yml
+rm -f _config.yml.tmp
+
 rsync --exclude=.git -av --del _site/ ../vakhov-test
 
 pushd ../vakhov-test
