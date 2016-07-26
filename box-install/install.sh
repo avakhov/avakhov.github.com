17385afdf86d67d9ea93349c76282b2ff941d87b
Alex <vakhov@gmail.com>
2016-07-26 17:23:26 -0400

    Published at 2016-07-26__17:23:26

--- box-install/install.sh
+++ box-install/install.sh
@@ -1,26 +1,23 @@
 #!/bin/bash
 HOST=root@b.vakhov.me
 
-ssh $HOST mkdir -p /root/.ssh
-scp box-install/id_rsa* $HOST:/root/.ssh
-ssh $HOST 'echo StrictHostKeyChecking=no > /root/.ssh/config'
-ssh $HOST 'test -d /root/blog || git clone git@github.com:avakhov/blog /root/blog'
-ssh $HOST git config --global user.email "vakhov@gmail.com"
-ssh $HOST git config --global user.name "Alex Vakhov"
-ssh $HOST git config --global push.default matching
+# ssh $HOST mkdir -p /root/.ssh
+# scp box-install/id_rsa* $HOST:/root/.ssh
+# ssh $HOST 'echo StrictHostKeyChecking=no > /root/.ssh/config'
+# ssh $HOST 'test -d /root/blog || git clone git@github.com:avakhov/blog /root/blog'
 
 # ruby-ruby-ruby - https://gist.github.com/scmx/9489499
-# ssh $HOST << EOF
-#   apt-get -y install build-essential zlib1g-dev libssl-dev libreadline6-dev libyaml-dev nodejs
-#   pushd /tmp
-#     wget http://cache.ruby-lang.org/pub/ruby/2.1/ruby-2.1.2.tar.gz
-#     tar -xvzf ruby-2.1.2.tar.gz
-#     pushd ruby-2.1.2/
-#       ./configure --prefix=/usr/local
-#       make
-#       make install
-#     popd
-#   popd
-#
-#   gem install bundler
-# EOF
+ssh $HOST << EOF
+  apt-get -y install build-essential zlib1g-dev libssl-dev libreadline6-dev libyaml-dev
+  pushd /tmp
+    wget http://cache.ruby-lang.org/pub/ruby/2.1/ruby-2.1.2.tar.gz
+    tar -xvzf ruby-2.1.2.tar.gz
+    pushd ruby-2.1.2/
+      ./configure --prefix=/usr/local
+      make
+      make install
+    popd
+  popd
+
+  gem install bundler
+EOF
